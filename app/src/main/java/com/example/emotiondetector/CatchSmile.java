a442909c4d14996d3e71e450ebb6f7090b90c46a
Eugeniusz Gronski <eugenegronski@gmail.com>
2020-11-25 23:39:23 +0100

    little progress

--- app/src/main/java/com/example/emotiondetector/CatchSmile.java
+++ app/src/main/java/com/example/emotiondetector/CatchSmile.java
@@ -1,14 +1,199 @@
 package com.example.emotiondetector;
 
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Rect;
 import android.os.Bundle;
+import android.util.Size;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.camera.core.Camera;
+import androidx.camera.core.CameraSelector;
+import androidx.camera.core.ImageAnalysis;
+import androidx.camera.core.ImageCapture;
+import androidx.camera.core.ImageProxy;
+import androidx.camera.core.Preview;
+import androidx.camera.lifecycle.ProcessCameraProvider;
+import androidx.camera.view.PreviewView;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.lifecycle.LifecycleOwner;
+
+import com.example.emotiondetector.Helper.GraphicOverlay;
+import com.example.emotiondetector.Helper.RectOverlay;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.common.util.concurrent.ListenableFuture;
+import com.google.mlkit.vision.common.InputImage;
+import com.google.mlkit.vision.face.Face;
+import com.google.mlkit.vision.face.FaceDetection;
+import com.google.mlkit.vision.face.FaceDetector;
+import com.google.mlkit.vision.face.FaceDetectorOptions;
+
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
 
 public class CatchSmile extends AppCompatActivity {
 
-    @Override
+    //variables from Activity
+    Button mButtonCaptureImage = null;
+    //graphic overlay class
+    GraphicOverlay graphicOverlay;
+
+
+    private Executor executor = Executors.newSingleThreadExecutor();
+    private int REQUEST_CODE_PERMISSIONS = 1001;
+    private final String[] REQUIRED_PERMISSIONS = new String[]{"android.permission.CAMERA", "android.permission.WRITE_EXTERNAL_STORAGE"};
+
+    PreviewView mPreviewView;
+    ImageButton mToggleBtn;
+    private ImageCapture imageCapture;
+
+    private int toggle = 0;
+
+    private ListenableFuture<ProcessCameraProvider> cameraProviderFuture;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_catch_smile);
+
+        //mButtonCaptureImage = findViewById(R.id.camera_capture_button);
+        //graphicOverlay =findViewById(R.id.textureViewCamera);
+        mPreviewView = findViewById(R.id.view_finder);
+        mToggleBtn = findViewById(R.id.toggle_button);
+
+        mToggleBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+
+        if(allPermissionsGranted()){
+            cameraProviderFuture = ProcessCameraProvider.getInstance(this);
+            cameraProviderFuture.addListener(() -> {
+                try {
+                    ProcessCameraProvider cameraProvider = cameraProviderFuture.get();
+                    bindPreview(cameraProvider);
+                } catch (ExecutionException | InterruptedException e) {
+                    // No errors need to be handled for this Future.
+                    // This should never be reached.
+                }
+            }, ContextCompat.getMainExecutor(this));
+
+            //startCamera(); //start camera if permission has been granted by user
+        } else{
+            ActivityCompat.requestPermissions(this, REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS);
+        }
+
+
     }
+
+    private void bindPreview(ProcessCameraProvider cameraProvider) {
+
+        ImageAnalysis imageAnalysis =
+                new ImageAnalysis.Builder()
+                        .setTargetResolution(new Size(1280, 720))
+                        .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
+                        .build();
+
+        imageAnalysis.setAnalyzer(executor, new ImageAnalysis.Analyzer() {
+            @Override
+            public void analyze(@NonNull ImageProxy image) {
+                int rotationDegrees = image.getImageInfo().getRotationDegrees();
+                //Toast.makeText(CatchSmile.this, "analizer is ready", Toast.LENGTH_LONG);
+            }
+        });
+
+
+        Preview preview = new Preview.Builder()
+                .build();
+
+        CameraSelector cameraSelector = new CameraSelector.Builder()
+                .requireLensFacing(CameraSelector.LENS_FACING_BACK)
+                .build();
+
+        preview.setSurfaceProvider(mPreviewView.getSurfaceProvider());
+
+        Camera camera = cameraProvider.bindToLifecycle((LifecycleOwner)this, cameraSelector,imageAnalysis, preview);
+    }
+
+
+    //processFace
+    private void processFaceDetection(byte[] byteArray) {
+
+        Bitmap bitmap = BitmapFactory.decodeByteArray(byteArray,0,byteArray.length);
+        InputImage image = InputImage.fromBitmap(bitmap, 0);
+        // Real-time contour detection of multiple faces
+        FaceDetectorOptions options =
+                new FaceDetectorOptions.Builder()
+                        .setPerformanceMode(FaceDetectorOptions.PERFORMANCE_MODE_FAST)
+                        .setContourMode(FaceDetectorOptions.LANDMARK_MODE_NONE)
+                        .setClassificationMode(FaceDetectorOptions.CLASSIFICATION_MODE_NONE)
+                        .build();
+
+
+        FaceDetector faceDetector = FaceDetection.getClient(options);
+
+        Task<List<Face>> task = faceDetector.process(image);
+        task.addOnSuccessListener(new OnSuccessListener<List<Face>>() {
+            @Override
+            public void onSuccess(List<Face> faces) {
+                getFaceResults(faces);
+                faceDetector.close();
+            }
+            }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(CatchSmile.this, "Error: " + e.getMessage(), Toast.LENGTH_LONG);
+                    }
+                });
+
+    }
+
+    private void getFaceResults(List<Face> faces) {
+        int counter = 0;
+        for(Face face : faces){
+            Rect rect = face.getBoundingBox();
+            RectOverlay rectOverlay = new RectOverlay(graphicOverlay, rect);
+            counter++;
+
+            graphicOverlay.add(rectOverlay);
+        }
+    }
+
+
+//    @Override
+//    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+//        //start camera when permissions have been granted otherwise exit app
+//        if(requestCode == REQUEST_CODE_PERMISSIONS){
+//            if(allPermissionsGranted()){
+//                startCamera();
+//            } else{
+//                Toast.makeText(this, "Permissions not granted by the user.", Toast.LENGTH_SHORT).show();
+//                finish();
+//            }
+//        }
+//    }
+
+    private boolean allPermissionsGranted(){
+        //check if req permissions have been granted
+        for(String permission : REQUIRED_PERMISSIONS){
+            if(ContextCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED){
+                return false;
+            }
+        }
+        return true;
+    }
+
 }
--- app/src/main/java/com/example/emotiondetector/MainActivity.java
+++ app/src/main/java/com/example/emotiondetector/MainActivity.java
@@ -1,11 +1,14 @@
 package com.example.emotiondetector;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -42,6 +45,16 @@ public class MainActivity extends AppCompatActivity {
                 openFaceRecognitionActivity();
             }
         });
+
+
+        //request for permissions from user
+        if (checkSelfPermission(Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED
+                ||
+                (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED)
+        )
+        {
+            requestPermissions(new String[]{Manifest.permission.CAMERA,Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+        }
     }
 
     private void openFaceRecognitionActivity() {
